5e86ecd347992dec73d5bdfe0f15ecb5db266b14
dswensen95 <46766071+dswensen95@users.noreply.github.com>
2021-10-29 10:13:03 -0700

    Update main.js

--- lesson7/main.js
+++ lesson7/main.js
@@ -1,1 +1,46 @@
+article img[data-src] {
+  filter: blur(0.2em);
+}
 
+article img {
+  filter: blur(0em);
+  transition: filter 0.5s;
+}
+
+const images = document.querySelectorAll("[data-src]");
+
+function preloadImage(img){
+  const src = img.getAttribute("data-src");
+  if (!src){
+    return;
+  }
+  else {
+    img.src = src;
+  }
+}
+
+const imgOptions = {
+  threshold = 0,
+  rootMargin = "0px 0px 300px 0px";
+  
+};
+
+const imgObserver = new intersectionObserver((entries, imgObserver) => {
+
+  entries.forEach(entry => {
+   
+    if (!entry.isIntersecting)
+    {
+      return;
+    } else {
+      preloadImage(entry.target);
+      imageObserver.unobserve(entry.target);
+    }
+    
+  }
+
+}, imgOptions);
+  
+  images.forEach(image => {
+    imageObserver.observe(image);
+  });
