6b341f11cfc11f47a767b634348dd65ac8b5ac93
dswensen95 <46766071+dswensen95@users.noreply.github.com>
2021-11-09 17:26:45 -0700

    Update prophets.js

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -3,17 +3,30 @@ fetch(requestURL)
   .then(function (response) {
     return response.json();
   })
+
   .then(function (jsonObject) {
-    console.table(jsonObject);  // temporary checking for valid response and data parsing
-  for (let i = 0; i < prophets.length; i++ ) {
+      console.table(jsonObject);  // temporary checking for valid response and data parsing
+      const prophets = jsonObject['prophets'];
+      for (let i = 0; i < prophets.length; i++ ){
+
+        let card = document.createElement('section');
+        let h2 = document.createElement('h2');
+        let p = document.createElement('p');
+        let p2 = document.createElement('p');
+        let image = document.createElement('img');
+        h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+        p.textContent = 'Date of Birth: ' + prophets[i].birthdate;
+        p2.textContent = 'Birthplace: ' + prophets[i].birthplace;
+        card.appendChild(h2);
+        card.appendChild(p);
+        card.appendChild(p2);
+        card.appendChild(image);
+        
+        document.querySelector('div.cards').appendChild(card);
+
+        image.setAttribute('src', prophets[i].imageurl);
+
+      }
+
+
   });
-const prophets = jsonObject['prophets'];
-  
- let card = document.createElement('section');
-let h2 = document.createElement('h2');
-
-h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-
-card.appendChild(h2);
-
-document.querySelector('div.cards').appendChild(card);
